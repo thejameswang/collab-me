cd9a4dbe68919fb5f288bbc4dd6f03f84d8d3598
katslump <kat.slump@gmail.com>
2018-03-19 16:19:29 -0700

    Added add document form

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Main from './components/Main.jsx'
 import Documents from './components/Documents.jsx'
+import AddDocument from './components/AddDocument.jsx'
 
 export default class App extends React.Component {
   render() {
     return (<div>
       <Main/>
       <Documents/>
+      <AddDocument/>
     </div>);
   }
 }
---
+++ src/components/AddDocument.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class AddDocument extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (<div>
+            <h1>Add Document</h1>
+            <input/>
+        </div>);
+    }
+}
