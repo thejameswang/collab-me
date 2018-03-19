eff725c446aeee701d7f399b45fa96ced60ed3f2
James Wang <j.wang5441@gmail.com>
2018-03-19 11:23:27 -0700

    changes linter, adds more dependencies

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Editor, EditorState } from 'draft-js';
 
 export default class App extends React.Component {
   render() {
     return (<div>
-      <h2>Welcome to React!</h2>
+      <h2>You're Dumb!</h2>
     </div>);
   }
 }
