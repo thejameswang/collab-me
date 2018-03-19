0c9ee2440ee385741bec53088ac7644bd21167ef
James Wang <j.wang5441@gmail.com>
2018-03-19 15:14:53 -0700

    creates paragraph alignment functionality

--- src/components/draft.jsx
+++ src/components/draft.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import { Editor, EditorState, RichUtils} from 'draft-js';
 // import '../stylesheets/styles.css';
 
+// function myBlockStyleFn(contentBlock) {
+//   const type = contentBlock.getType();
+//   if (type === 'blockquote') {
+//     return 'superFancyBlockquote';
+//   }
+// }
+
+
+// const getBlockStyle = (block) => {
+//   switch (block.getType()) {
+//       case 'left':
+//           return 'align-left';
+//       case 'center':
+//           return 'align-center';
+//       case 'right':
+//           return 'align-right';
+//       default:
+//           return null;
+//   }
+// }
+
+
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -19,16 +41,34 @@ export default class Main extends React.Component {
     return 'not-handled';
   }
 
+
   _onBoldClick() {
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
   }
-
   _onItalicsClick() {
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
   }
+  _onUnderlineClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+  _onStrikethroughClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'STRIKETHROUGH'));
+  }
+  _onLeftAlignClick() {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'STRIKETHROUGH'));
+  }
+  _onRightAlignClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ALIGNRIGHT'));
+  }
+  _onLeftAlignClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ALIGNLEFT'));
+  }
+  _onCenterAlignClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ALIGNCENTER'));
+  }
+
 
   render() {
-
     return (<div className="container">
         <p>
             <button className="btn btn-xs btn-default" title="back"><i className="fa fa-angle-left"></i> Go Back</button>
@@ -40,20 +80,21 @@ export default class Main extends React.Component {
             <h5>Document Name</h5>
             <p>Shareable ID: #########<button className="btn btn-xs btn-default" title="copy"><i className="fa fa-copy"></i> Copy to Clipboard</button></p>
         </div>
+
       <div className='btn-group'>
           <button className="btn btn-xs btn-default" title="bold" onClick={this._onBoldClick.bind(this)}><i className="fa fa-bold"></i></button>
           <button className="btn btn-xs btn-default" title="italic" onClick={this._onItalicsClick.bind(this)}><i className="fa fa-italic"></i></button>
-          <button className="btn btn-xs btn-default" title="underline"><i className="fa fa-underline"></i></button>
-          <button className="btn btn-xs btn-default" title="strikethrough"><i className="fa fa-strikethrough"></i></button>
+          <button className="btn btn-xs btn-default" title="underline" onClick={this._onUnderlineClick.bind(this)} ><i className="fa fa-underline"></i></button>
+          <button className="btn btn-xs btn-default" title="strikethrough" onClick={this._onStrikethroughClick.bind(this)}><i className="fa fa-strikethrough"></i></button>
       </div>
       <div className='btn-group'>
           <button className="btn btn-xs btn-default" title="font-color"><i className="fa fa-paint-brush"></i></button>
           <button className="btn btn-xs btn-default" title="font-size"><i className="fa fa-font"></i></button>
       </div>
       <div className='btn-group'>
-          <button className="btn btn-xs btn-default" title="left-align"><i className="fa fa-align-left"></i></button>
-          <button className="btn btn-xs btn-default" title="center-align"><i className="fa fa-align-justify"></i></button>
-          <button className="btn btn-xs btn-default" title="right-align"><i className="fa fa-align-right"></i></button>
+          <button className="btn btn-xs btn-default" onClick={this._onLeftAlignClick.bind(this)} title="left-align"><i className="fa fa-align-left"></i></button>
+          <button className="btn btn-xs btn-default" onClick={this._onCenterAlignClick.bind(this)} title="center-align"><i className="fa fa-align-justify"></i></button>
+          <button className="btn btn-xs btn-default" onClick={this._onRightAlignClick.bind(this)} title="right-align"><i className="fa fa-align-right"></i></button>
       </div>
       <div className='btn-group'>
           <button className="btn btn-xs btn-default" title="bulleted-list"><i className="fa fa-list-ul"></i></button>
@@ -61,7 +102,7 @@ export default class Main extends React.Component {
       </div>
           <button className="btn btn-xs btn-default" title="custom">Custom</button>
       <div className="editor">
-          <Editor editorState={this.state.editorState} handleKeyCommand={this.handleKeyCommand} onChange={this.onChange}/>
+          <Editor customStyleMap={styleMap} editorState={this.state.editorState} handleKeyCommand={this.handleKeyCommand} onChange={this.onChange}/>
       </div>
       <p>
           <button className="btn btn-xs btn-default" title="save">Save Changes</button>
@@ -69,3 +110,21 @@ export default class Main extends React.Component {
     </div>);
   }
 }
+
+const styleMap = {
+  'ALIGNRIGHT': {
+    textAlign: 'right',
+    display: 'inline-block',
+    width: '100%'
+  },
+  'ALIGNLEFT': {
+    textAlign: 'left',
+    display: 'inline-block',
+    width: '100%'
+  },
+  'ALIGNCENTER': {
+    textAlign: 'center',
+    display: 'inline-block',
+    width: '100%'
+  },
+};
