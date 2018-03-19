20f31982f1cece1142fab5f5dc07263c9cb11934
James Wang <j.wang5441@gmail.com>
2018-03-19 11:57:20 -0700

    created bolding and richtext input component

---
+++ src/components/draft.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Editor, EditorState, RichUtils} from 'draft-js';
+// import '../stylesheets/styles.css';
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
+  render() {
+    return (<div className="">
+      <h2>You're Dumb!</h2>
+      <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+      <div className="DottedBox">
+          <Editor editorState={this.state.editorState} handleKeyCommand={this.handleKeyCommand} onChange={this.onChange}/>
+      </div>
+    </div>);
+  }
+}
