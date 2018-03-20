1edb45ea3176f183d2f8f1e3160493d08c431ce1
James Wang <j.wang5441@gmail.com>
2018-03-19 21:15:41 -0700

    finishes authentication and routes for register and login

--- backend/index.js
+++ backend/index.js
@@ -7,28 +7,68 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import initializeAuth from './routes/auth'
 import databaseAccess from './routes/databaseAccess'
+import User from './models/User'
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(process.env.MONGODB_URI);
 
 databaseAccess(app);
 initializeAuth(app);
 
 
 //ROUTES ARE HERE
-app.post('/login', passport.authenticate('local', { successRedirect: '/',
-                                                    failureRedirect: '/login' }));
+
+app.post('/login', function(req, res, next) {
+	// Do email and password validation for the server
+	passport.authenticate("local", function(err, user, info) {
+
+		if(err) return next(err)
+		if(!user) {
+			return res.json({ success: false, message: info.message })
+		}
+
+		req.logIn(user, loginErr => {
+			if(loginErr) {
+				return res.json({ success: false, message: loginErr })
+			}
+			return res.json({ success: true, message: "authentication succeeded" })
+		})
+	})(req, res, next)
+});
+
+app.get("/logout", function(req, res,next) {
+  req.logout()
+	return res.json({ success: true })
+})
+
+app.post('/register', function(req, res, next) {
+  User.findOne({ username: req.body.username }, (err, user) => {
+		// is email address already in use?
+		if (user) {
+			res.json({ success: false, message: "Email already in use" })
+			return
+		}
+		// go ahead and create the new user
+		else {
+			User.create(req.body, (err) => {
+				if (err) {
+					console.error(err)
+					res.json({ success: false })
+					return
+				}
+				res.json({ success: true })
+				return
+			})
+		}
+	})
+})
 
 app.get('/', function(req,res) {
-  console.log(req.user)
-  // console.log(req)
-
   if(req.user) {
-    res.render('index', {
-      user: req.user
-    });
+    res.json()
   } else {
     res.redirect('/login')
   }
@@ -40,5 +80,3 @@ app.get('/', function(req,res) {
 
 console.log('Express started. Listening on port', process.env.PORT || 3000);
 app.listen(process.env.PORT || 3000);
-
-mongoose.connect(process.env.MONGODB_URI);
