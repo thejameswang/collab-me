d0334bd794e14441d98b920b9d0343d898d9d3b2
katslump <kat.slump@gmail.com>
2018-03-19 19:40:17 -0700

    Bug on login page fixed

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import Main from './components/Main.jsx'
-import Documents from './components/Documents.jsx'
-import AddDocument from './components/AddDocument.jsx'
-import ViewShared from './components/ViewShared.jsx'
+import Main from './components/Main.jsx';
+import Documents from './components/Documents.jsx';
+import AddDocument from './components/AddDocument.jsx';
+import ViewShared from './components/ViewShared.jsx';
+import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducers/index';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+
+let store = createStore(reducer);
+window.store = store;
 
 export default class App extends React.Component {
   render() {
-    return (<div>
+      return (
+        <Provider store={store}>
+            <Router>
+                <Route path="/" component={Login}/>
+            </Router>
+        </Provider>
+      )
+    return ( <div>
       <Main/>
       <Documents/>
       <AddDocument/>
